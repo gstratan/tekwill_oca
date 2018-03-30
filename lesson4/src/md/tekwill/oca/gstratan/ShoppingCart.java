081daa8e4e15088f179174bf3f234da90584cb10
Stratan Gheorghe <gstratan@gmail.com>
2018-03-30 19:48:02 +0300

    lectia 4

---
+++ lesson4/src/md/tekwill/oca/gstratan/ShoppingCart.java
@@ -0,0 +1,11 @@
+package md.tekwill.oca.gstratan;
+
+public class ShoppingCart {
+    public static void main (String[] arg) {
+        String custName = "Mary Smith";
+        String itemDesc = "Shirt";
+        String message;
+        message = custName + " vrea sa cumpere " +itemDesc;
+        System.out.println(message);
+    }
+}
